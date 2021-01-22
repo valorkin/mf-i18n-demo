a65c30ad98ca43d70888a4593b115adbd4712c95
spawn <sergey.melnik@valor-software.com>
2021-01-22 17:53:23 +0200

    fix: configure Dockerfile and Nginx conf files

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-docker build -t i18n-nginx .
+docker build -t i18n-nginx . --no-cache
 docker rm run-i18n-nginx -f
-docker run --name run-i18n-nginx -d i18n-nginx
+docker run -p 80:80 --name run-i18n-nginx -d i18n-nginx
 docker container ls
 docker logs run-i18n-nginx --follow
